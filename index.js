9cf7d12b408f99569b2717f2dcf7ee9ca1b38517
matiasbernardini <bernardinimatias14@gmail.com>
2022-06-17 20:58:11 -0300

    primer entrega

---
+++ index.js
@@ -0,0 +1,59 @@
+let nombre = prompt ("Ingrese su Nombre")
+let apellido = prompt ("Ingrese su Apellido")
+if (nombre !== "" && apellido !== ""){
+    alert ( `¡Bienvenido ${nombre} ${apellido}!`)
+} else{
+    alert ("Tiene que ingrsar su nombre y apellido")
+}
+
+
+let edad = parseInt (prompt ("Ingrese su Edad"))
+if (edad < 0 || edad > 99 ){
+    alert ("La edad no es correspondiente")
+}else { 
+    if (edad >=18){
+    alert ("Usted es Mayor de Edad")
+}
+    if (edad <18){
+    alert ("Usted es menor de edad")
+}
+}
+
+
+let paises = parseInt (
+    prompt (
+        "A cual seleccion apoyara para el mundial: \n 1.Argentina \n 2.Uruguay \n 3.Mexico \n 4.España"
+    )
+)
+if (isNaN (paises)){
+    alert ("Tiene que seleecionar un numero")
+} else{
+    if (paises === 1){
+        alert ("Usted va con Argentina")
+    } else if (paises ===2){
+        alert ("Usted va con Uruguay")
+    } else if (paises === 3){
+        alert ("Usted va con Mexico")
+    } else if (paises === 4){
+        alert ("Usted va con España")
+    } else {
+        alert ("Tiene que seleccionar una opcion")
+    }
+}
+
+
+function sumarGoles (golesUno, golesDos, golesTres){
+    let sumatoria = golesUno + golesDos + golesTres
+    return sumatoria
+}
+function golesPromedio (){
+    let golUno = parseInt (prompt ("Cuantos goles cree que metera su seleccion en el 1er partido"))
+    let golDos = parseInt (prompt ("Cuantos goles cree que metera su seleccion en el 2do partido"))
+    let golTres = parseInt (prompt ("Cuantos goles cree que metera su seleccion en el 3er partido"))
+
+    let resultadoGoles = sumarGoles (golUno, golDos, golTres)
+    let promedio = resultadoGoles
+
+    alert ( "La cantidad de goles que metera su seleccion en la fase de grupos es: " + promedio )
+}
+golesPromedio()
